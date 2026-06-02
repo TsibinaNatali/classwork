827079b2f3832aacc16677d11b73de77d8d83e07
Взрослая академия <georgygevorkov@gmail.com>
2026-06-02 21:07:56 +0300

    условия

---
+++ 2.06.2026.py
@@ -0,0 +1,39 @@
+# number = int(input("введите двухзначное число"))
+# num1 = number % 10
+# num2 = number // 10
+# print(num1 + num2)
+#
+# number2 = int(input("введите трехзначное число "))
+# num3 = number2 % 10
+# num4 = number2 % 100 // 10
+# num5 = number2 // 100
+#
+# print(num3 + num4 + num5)
+#
+#
+# number3 = int(input("введите черехзначное число "))
+# n1 = number3 % 10
+# n2 = number3 % 100 // 10
+# n3 = number3 // 100 % 10
+# n4 = number3 // 1000
+# print(f"{n1}{n2}{n3}{n4}")
+#
+# print((n1*1000+n2*100+n3*10+n4)*2)
+
+# num1 = int(input("введите 1-е число"))
+# num2 = int(input("введите 2-е число"))
+#
+# if num1 > num2:
+#     print(num1)
+# if num1 < num2:
+#     print(num2)
+# if num1 == num2:
+#     print("равные")
+
+
+name = input("введите имя")
+if name =="Ваня":
+    print(F"{name} верное")
+
+else:
+    print("имя неверное")
