20a82abd2c94db265502b4e56cb18cb993eb500d
Взрослая академия <georgygevorkov@gmail.com>
2026-06-11 21:07:49 +0300

    циклы for

---
+++ 11.06.2026.py
@@ -0,0 +1,95 @@
+# a=-1
+# b=10
+# while a < b:
+#     print(b)
+#     b -= 1
+#
+#
+# a1 = -1
+# b1 = 10
+# s1 = 0
+# while a1 < b1:
+#     print(b1)
+#     s1 += a1
+#     a1 -= 1
+# print(f"{s1} = cмма всех чисел")
+#
+#
+# n =1
+# i = 0
+# s = 0
+# while n > 0:
+#     n= int(input("введите число"))
+#     s+=n
+#     if n == 0:
+#         break
+# print(f"{s} = cмма всех чисел")
+
+
+# min1 = int(input("число от"))
+# max1 = int(input("число до"))
+# min2 = int(input("число от"))
+# max2 = int(input("число до"))
+# if min1>max1: min1,max1 = max1,min1
+# if min2>max2: min2,max2 = max2,min2
+
+# if min1 > min2:
+#     min = min1
+# else:
+#     min2
+# вместо if и else записываем так   min = min1>min2 and min1 or min2
+# if max1<max2:
+#     max = max1
+# else:
+#     max2
+# while min <= max:
+#     print(min, end=" ")
+#     min+=1
+#
+#
+#
+
+#
+# number = int(input("num"))
+#
+# print(number % 2 and "nechet" or "chet")
+
+# a = int(input("a"))
+# b= int(input("b"))
+# if a<b: a,b = b,a
+#
+# for i in range(a, b+1):
+#     print(i)
+#     print(a+b/2)
+#
+# a1 = int(input("a"))
+# b1= int(input("b"))
+# for i in range(a1, b1+1, 3):
+#     print(i)
+# itog=0
+# a2 = int(input("a"))
+# b2= int(input("b"))
+# for i in range(a2, b2+1, 1):
+#     print(f"{a2} * {b2} ={itog}")
+#
+#
+# mult=1
+# a2 = int(input("a"))
+# b2= int(input("b"))
+# for i in range(a2, b2+1, 1):
+#     mult*=i
+#     print(mult)
+#
+# flag= False
+# b3 = int(input("b"))
+# for i in range(5):
+#     if
+#
+# n = int(input("a"))
+# for i in range(n):
+#     num = int(input("a"))
+#     if num % 2 == 0:
+#         print(num)
+
+
+
