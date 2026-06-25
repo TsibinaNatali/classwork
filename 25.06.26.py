a72ec3939bbd94e20ef4e91dc9eff3f2d56c1abd
Взрослая академия <georgygevorkov@gmail.com>
2026-06-25 20:56:07 +0300

    циклы for

---
+++ 25.06.26.py
@@ -0,0 +1,86 @@
+# name = input("Введите ваше имя ")
+# age = int(input("Введите ваш возраст "))
+# print(f"Имя - {name}. Возраст - {age} лет. ",end="")
+# print()
+# a = int(input("Введите первое число "))
+# b = int(input("Введите второе число "))
+# c = int(input("Введите третье число "))
+# d = int(input("Введите четвертое число "))
+# if a > b and a > c and a > d:
+#     print(f"самое большое число - {a}")
+# elif b > c and b > d:
+#     print(f"самое большое число - {b}")
+# elif c > d:
+#     print(f"самое большое число - {c}")
+# else:
+#     print(f"самое большое число - {d}")
+# print()
+# s_square = 0
+# figura = int(input("Выберите фигуру: 1-квадрат, 2 - прямоугольник "))
+# if figura == 1:
+#     square = int(input("чему равна сторона квадрата "))
+#     s_square = square* square
+#     print(f"площадь квадрата = {s_square}")
+# elif figura == 2:
+#     rectangle_a = int(input("чему равна первая сторона прямоугольника "))
+#     rectangle_b = int(input("чему равна вторая сторона прямоугольника "))
+#     s_rectangle = rectangle_a * rectangle_b
+#     print(f"площадь прямоугольника = {s_rectangle}")
+# print()
+#
+# a = int(input("Введите первое число "))
+# b = int(input("Введите второе число "))
+# for i in range (a , b+1, 1):
+#     print(i)
+print()
+# num = 1
+# sum = 0
+# work = 1
+# while num != 0:
+#     num = int(input("Введите  число "))
+#     sum += num
+#     if num == 0:
+#         continue
+#     work *= num
+# print(f"сумма чисел = {sum}")
+# print(f"произведение чисел = {work}")
+# print()
+# number_a = int(input("Введите первое число "))
+# number_b = int(input("Введите второе число "))
+# for i in range(number_b, number_a-1,-1):
+#     if i % 3 == 0:
+#         print(i)
+# print()
+
+# for i in range(1, 10 , 1):
+#     for j in range(1, 10, 1):
+#         work = i * j
+#         print(f"{i} * {j} = {work} ")
+#     print()
+# print()
+
+
+
+# n = int(input("Cколько чисел хотите ввести "))
+#numbers = int(input("Введите число "))
+# max_numbers = numbers
+# min_numbers = numbers
+# for i in range(n-1):
+#     numbers = int(input("Введите число "))
+#     if max_numbers < numbers:
+#         max_numbers = numbers
+#
+#     if min_numbers > numbers:
+#         min_numbers = numbers
+# print(f"{min_numbers} - минимальное число ")
+# print(f"{max_numbers} - максимальное число ")
+# print()
+
+
+storona = int(input("введите сторону квадрата "))
+for i in range(1, (storona+1)//2, 1):
+    for j in range( i,):
+        print("* ",end=" ")
+    print()
+
+
